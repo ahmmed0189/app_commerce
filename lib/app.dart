b1a8f7ca7b4aeb90bc92ea343826bf0a704de587
dein Github username <deineGithubEMail@mail.de>
2024-06-27 16:00:12 +0200

    auth

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:app_commerce/src/data/auth_repository.dart';
 import 'package:app_commerce/src/data/database_repository.dart';
 import 'package:app_commerce/src/features/cart/presentation/cart_page.dart';
 import 'package:app_commerce/src/features/registration/presentation/login_screen.dart';
@@ -5,21 +6,30 @@ import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   final DatabaseRepository repo;
-  App({required this.repo, super.key});
+  final AuthRepository authRepository;
+
+  const App({required this.repo, super.key, required this.authRepository});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData.dark(),
-      home: LoginScreen(repo: repo),
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.amber,
-      ),
-      routes: {
-        '/cart_page': (context) => CartPage(repo: repo),
-      },
-    );
+    return StreamBuilder(
+        stream: authRepository.authStateChanges(),
+        builder: (context, snapshot) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            darkTheme: ThemeData.dark(),
+            home: LoginScreen(
+              repo: repo,
+              authRepository: authRepository,
+            ),
+            theme: ThemeData(
+              brightness: Brightness.dark,
+              primarySwatch: Colors.amber,
+            ),
+            routes: {
+              '/cart_page': (context) => CartPage(repo: repo),
+            },
+          );
+        });
   }
 }
